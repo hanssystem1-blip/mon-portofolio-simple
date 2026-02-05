f456a2eb40e9beeb21bcfc9f70df00d2b2d4f8b9
hanssystem <hanssystem1@gmail.com>
2026-02-05 16:08:41 +0100

    modication du responsive

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 $(document).ready(function () {
     $('.dark').click(function () {
         $('footer').toggleClass('fond');
-
         $('body,nav').toggleClass('active');
         $('ion-icon').toggleClass('change');
-        $('header,section,p,').toggleClass('blanc');
+        $('header,section,p').toggleClass('blanc');
+        // $('article').toggleClass('rot');
     });
 });
+function rotate() {
+    rot = document.getElementById('change')
+    rot.style.transform = 'rotate(360deg)';
+    rot.style.transition = 'transform  1s ease-in-out';
+    // document.getElementById('change').addEventListener(
+    //     "click", () => {
+    //         n=100;
+    //         i=0;
+    //         if(i<=n)
+    //         {rot.style.transform = 'rotate(360deg)';
+    //         rot.style.transition = 'transform  1s ease infinite';}
+    //     }
+    // );
+}
 // rot = document.getElementById('change');
-    // rot.style.color = 'white';
+// rot.style.color = 'white';
 
 // document.getElementById('change').addEventListener(
 //     "click" ,()=>{
 //         rot = document.getElementById('change');
-//     rot.style.color = 'white';
+//         rot.style.transform = "rotate('360deg')";
+
 
 //     }
 // );
